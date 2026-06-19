32e68349ca0726df0c88436c3ec0d99d131d1c8d
Deepal <deepalece@gmail.com>
2026-06-19 20:15:34 +0530

    header file changed

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "./ThemeContext";
+import { ThemeContext } from "./Themecontext";
 
 function Header() {
   const theme = useContext(ThemeContext);
